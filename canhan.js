8dd35213eead5a840230c72825d80f4f5c1a688d
minhtri643 <162124917+minhtri643@users.noreply.github.com>
2024-09-17 09:15:00 +0700

    Delete canhan.js

--- canhan.js
+++
@@ -1,9 +0,0 @@
-// Hàm mở link bài tập trong tab mới
-function openInNewTab(link) {
-    window.open(link, '_blank').focus();
-}
-
-// Hàm mở link liên lạc trong tab mới
-function openLink(url) {
-    window.open(url, '_blank').focus();
-}
