9835d3ba95c12b995ee1d8f725e005078a54250d
minhtri643 <162124917+minhtri643@users.noreply.github.com>
2024-09-17 09:15:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Hàm mở link bài tập trong tab mới
+function openInNewTab(link) {
+    window.open(link, '_blank').focus();
+}
+
+// Hàm mở link liên lạc trong tab mới
+function openLink(url) {
+    window.open(url, '_blank').focus();
+}
